29666b49e7323c34df0f796ed5d9518e195f0be6
August49 <augustiniusngongo@gmail.com>
2024-04-10 17:07:47 -0400

    Registration view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -39,14 +39,30 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
-
+def registration(request):
+    # Get user information from request.POST dictionary
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    email = data['email']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    # Try to create a new user
+    try:
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        data = {"userName": username, "status": "User created"}
+    except Exception as e:
+        data = {"userName": username, "status": "Failed to create user"}
+    return JsonResponse(data)
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
